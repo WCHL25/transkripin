fec204849e79cc628a3d0c855c2cf696f6471ab1
Ahmad Muhajir <amuhajir.syamlan@gmail.com>
2025-09-22 18:56:15 +0700

    fix(frontend): wrong import script path after build

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'url';
 import environment from 'vite-plugin-environment';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react(), environment('all', { prefix: 'CANISTER_' }), environment('all', { prefix: 'DFX_' }), tailwindcss()],
   envDir: '../',
   define: {
